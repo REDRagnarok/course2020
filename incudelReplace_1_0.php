bc3ebcec30690b5b66750cd829f079ff220270e8
REDRagnarok <red-ragnarok@yandex.ru>
2020-02-20 21:51:59 +0300

    My homework task 4+ with tutor's notes.

--- incudelReplace_1_0.php
+++ incudelReplace_1_0.php
@@ -1,83 +1,80 @@
 <?php
 
-echo '<pre>';
+define('DS', DIRECTORY_SEPARATOR);
 $cDir = __DIR__;
-$sePar = DIRECTORY_SEPARATOR;
-$nDir = $cDir.$sePar.'_cache';
+$cacheDir = $cDir . DS . '_cache';
 
-if (!file_exists($nDir)){
-    if (!mkdir($nDir)){
+if (!file_exists($cacheDir)) {
+    if (!mkdir($cacheDir)) {
         die ('Не удалось создать директорию');
     }
 }
 
 // Копируем файлы в директорию '_cache'
-
 $dh = opendir($cDir);
-while (false !==($entry = readdir($dh))){
+while ($entry = readdir($dh)) {
+    $aFile = $cDir . DS . $entry;
 
-    $aFile = $cDir.$sePar.$entry;
-
-    if (is_dir($aFile)){
+    if (is_dir($aFile)) {
         continue;
     }
 
-    if ($aFile == __FILE__){
+    if ($aFile === __FILE__) {
         continue;
     }
 
-    if (pathinfo($aFile, PATHINFO_EXTENSION) == 'php'){
-         copy($aFile,$nDir.$sePar.$entry);
+    if (pathinfo($aFile, PATHINFO_EXTENSION) === 'php') {
+        copy($aFile, $cacheDir . DS . $entry);
     }
 }
 closedir($dh);
-echo "Copying done!\n";
+echo "Copying done!" . PHP_EOLs;
 
 // Ищем файлы с include в каталоге '_cache'
-
-$ch = opendir($nDir);
-while (false !==($entry = readdir($ch))) {
-    $substDone = false;
-    $rFile = file($nDir.$sePar.$entry);
-    $inclFound = preg_grep("/include '.*'/",$rFile); // Определяем наличие 'include' в файле
-//    print_r($inclFound)."\n";
-
-    while (!empty($inclFound)){
-
-        $key = array_key_first($inclFound);
-        $filename = explode("'",$inclFound[$key]); // Определяем имя подключаемого файла
-        $filename = $nDir.$sePar.$filename[1];
-//        echo $filename[1]."\n";
-        if (file_exists($filename)) {
-            echo "File to include ".$filename."\n";
-
-            $wFile = file($filename);unset($wFile[0]);   // Убираем тег '<?php' из начала файла
-            array_splice($rFile,$key,1,$wFile);
-            $inclFound = preg_grep("/include '.*'/",$rFile); // Обновляем массив значений 'include'
-        }
-        else{                                               // Производим замену 'include' содержимым файла
-            echo "File to include not found!\n";
-            unset($inclFound[$key]);                        // Удаляем запись о ненайденном файле из массива
-        }
-
-        $substDone = true;
+$hasInclude = false;
+$ch = opendir($cacheDir);
+while ($entry = readdir($ch)) {
+    $rFile = file($cacheDir . DS . $entry);
+    $inclFound = preg_grep("/include '.*'/", $rFile); // Определяем наличие 'include' в файле
+    if (empty($inclFound)) {
+        continue;
     }
 
-    if ($substDone){                                        // Проверяем, производились ли замены.
-        $fh = fopen($nDir.$sePar.$entry,'w');
-        foreach($rFile as $line){
-            fwrite($fh,$line);
+    $substDone = false;
+    while (!empty($inclFound)) {
+        $key = array_key_first($inclFound);
+        $filename = explode("'", $inclFound[$key]); // Определяем имя подключаемого файла
+        $filename = $cacheDir . DS . $filename[1];
+
+        if (!file_exists($filename)) {
+            echo "File to include not found!" . PHP_EOL;
+            unset($inclFound[$key]);                        // Удаляем запись о ненайденном файле из массива
+            continue;
+        }
+
+        $hasInclude = $substDone = true;
+        echo "File to include " . $filename . PHP_EOL;
+
+        $wFile = file($filename);
+        unset($wFile[0]);   // Убираем тег '<?php' из начала файла
+        array_splice($rFile, $key, 1, $wFile); // Производим замену 'include' содержимым файла
+        $inclFound = preg_grep("/include '.*'/", $rFile); // Обновляем массив значений 'include'
+    }
+
+    if ($substDone) {   // Проверяем, производились ли замены.
+        $fh = fopen($cacheDir . DS . $entry, 'w');
+        foreach ($rFile as $line) {
+            fwrite($fh, $line);
         }
         fclose($fh);
         echo "Search and replace  done!";
     }
 }
 closedir($ch);
-if ($substDone === false){
+
+if ($hasInclude === false) {
     echo "No files to include.\n";
 }
 
-
-
-
-
+//NOTE: за задание +++, можно кое где оптимизировать, например не пересматривать весь файл на include, а только добавляемые строки, также надо поработать над регулярками - они не сработают для html файлов внутри которых include, а по сути - это задание как раз на кеширование составных шаблонов
+//NOTE: алгоритмическая ошибка - проверка переменной $substDone после цикла в котором она инициализируется - буде выведено занчение только для последней итерации
